ea1704fc2c953f71ce6020b3ea7f6afd8f9c1599
Jhonathan Abreu <jdabreu25@gmail.com>
2025-12-22 12:51:28 -0400

    Minor change

--- QuantConnect.AlpacaBrokerage/AlpacaBrokerage.cs
+++ QuantConnect.AlpacaBrokerage/AlpacaBrokerage.cs
@@ -901,12 +901,7 @@ namespace QuantConnect.Brokerages.Alpaca
                     information.Add("organizationId", organizationId);
                 }
                 // Create HTTP request
-                var request = new HttpRequestMessage(HttpMethod.Post, "modules/license/read");
-                request.Content = new StringContent(
-                    JsonConvert.SerializeObject(information),
-                    Encoding.UTF8,
-                    "application/json"
-                );
+                using var request = ApiUtils.CreateJsonPostRequest("modules/license/read", information);
                 api.TryRequest(request, out ModulesReadLicenseRead result);
                 if (!result.Success)
                 {
